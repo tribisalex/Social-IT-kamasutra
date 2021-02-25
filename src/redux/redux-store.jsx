a30bf7c661b14b314ddaf7b92e2885ceb725941c
Vanya Korobeynikov <INKorobeynikov@alfabank.ru>
2021-02-25 22:29:04 +0300

    фиксы

--- src/redux/redux-store.jsx
+++ src/redux/redux-store.jsx
@@ -7,7 +7,7 @@ import authReducer from "./auth-reducer";
 import thunkMiddleWare from "redux-thunk";
 import { reducer as formReducer } from 'redux-form';
 
-let reducers = combineReducers ({
+const reducers = combineReducers ({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebarP: sidebarReducer,
@@ -16,6 +16,6 @@ let reducers = combineReducers ({
     form: formReducer
 });
 
-let store = createStore (reducers, applyMiddleware(thunkMiddleWare));
+const store = createStore(reducers, applyMiddleware(thunkMiddleWare));
 
 export default store;
