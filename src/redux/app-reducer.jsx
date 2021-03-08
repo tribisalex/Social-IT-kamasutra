9670ff20064b50836af4b1f103761a714d060551
TribisAlex <tribisalexandr@yandex.ru>
2021-03-08 16:40:56 +0300

    recelect

---
+++ src/redux/app-reducer.jsx
@@ -0,0 +1,63 @@
+import users from "../Components/Users/Users";
+import {authApi, userApi} from "../api/api";
+import {stopSubmit} from "redux-form";
+
+const SET_USER_DATA = 'SET_USER_DATA';
+
+let initialState = {
+    userId: null,
+    email: null,
+    login: null,
+    isAuth: false
+}
+
+const authReducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case SET_USER_DATA:
+            return {
+                ...state,
+                ...action.payload
+            }
+
+        default:
+            return state;
+    }
+}
+
+export const setAuthUserData = (userId, email, login, isAuth) => ({type: SET_USER_DATA, payload:
+        {userId, email, login, isAuth}  });
+
+export const getAuthUserData = () => (dispatch) => {
+        authApi.me()
+            .then(response => {
+                if (response.data.resultCode === 0) {
+                    let {id, email, login} = response.data.data;
+                    dispatch(setAuthUserData(id, email, login, true));
+                }
+            });
+    }
+
+
+export const login = (email, password, rememberMe) => (dispatch) => {
+    authApi.login(email, password, rememberMe)
+        .then(response => {
+            if (response.data.resultCode === 0) {
+                dispatch(getAuthUserData())
+                 } else {
+                let message = response.data.messages.length > 0 ? response.data.messages[0] : "Some Error";
+                dispatch(stopSubmit("login", {_error: message}));
+            }
+        });
+}
+
+export const logout = () => (dispatch) => {
+    authApi.logout()
+        .then(response => {
+            if (response.data.resultCode === 0) {
+                dispatch(setAuthUserData(null,null, null,false));
+            }
+        });
+}
+
+export default authReducer;
